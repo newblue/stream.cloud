c480b3437a5506c9e47bf6c71c04c2095b137ac6
newblue <newblue@gmail.com>
2011-05-02 21:36:48 +0800

    Add main.py to run wsgi and handle any request

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-import web
-import template
-
-class helloworld:
-    def GET (self):
-        return 'hello world'
-
-URLS = (
-        '/', 'helloworld'
-        )
-
-def main ():
-    application = web.application (URLS, globals ())
-    return application.cgirun ()
-
+import stream
 
 if __name__ == '__main__' :
-    main ()
+    stream.run ()
 
 
