bacbc14a98ec56f1747bec0b961d47d99fe5044e
newblue <newblue@gmail.com>
2011-05-03 12:01:36 +0800

    Tidy files

--- template.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
-__ALL__ = ['get']
-from jinja2 import Environment, FileSystemLoader 
-
-env = Environment (loader = FileSystemLoader ('../templates/'))
-get = env.get_template
